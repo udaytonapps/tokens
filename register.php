ba1751a8d4682964ca1880b607dae8a5183e7ea3
Andrew Niekamp <aniekamp1@udayton.edu>
2022-06-21 15:45:13 -0400

    Initial commit of register.php

---
+++ register.php
@@ -0,0 +1,22 @@
+<?php
+
+$REGISTER_LTI2 = array(
+    "name" => "Tokens",
+    "FontAwesome" => "fa-coins",
+    "short_name" => "Tokens",
+    "description" => "Create a token economy for your classroom",
+    "messages" => array("launch", "launch_grade"),
+    "license" => "Apache",
+    "languages" => array(
+        "English"
+    ),
+    "analytics" => array(
+        "internal"
+    ),
+    "tool_phase" => "new",
+    "video" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "source_url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    // "screen_shots" => array(
+    //     "",
+    // )
+);
