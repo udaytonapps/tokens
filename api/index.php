d6ed12035013c932bfe09b46b078d25a812f44eb
Andrew Niekamp <aniekamp1@udayton.edu>
2022-06-28 10:51:26 -0400

    TSUGI-346 Update config to use common service

--- api/index.php
+++ api/index.php
@@ -8,8 +8,8 @@ $LAUNCH = LTIX::requireData(array(LTIX::CONTEXT, LTIX::LINK));
 // Import the router
 require_once __DIR__ . '/_router/index.php';
 
-// Import each feature that will be used
-// require_once __DIR__ . '/common/index.php';
+// Import each resource that will be used
+require_once __DIR__ . '/common/index.php';
 require_once __DIR__ . '/instructor/index.php';
 // TODO: learner
 // require_once __DIR__ . '/learner/index.php';
