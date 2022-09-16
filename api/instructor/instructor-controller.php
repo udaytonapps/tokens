330e4e906ec1a8043f25b30a5779ae190720a054
Andrew Niekamp <aniekamp1@udayton.edu>
2022-09-16 14:06:28 -0400

    TSUGI-407 Update for keeping roster in common service

--- api/instructor/instructor-controller.php
+++ api/instructor/instructor-controller.php
@@ -110,11 +110,9 @@ class InstructorCtr
         $requests = self::$DAO->getCourseRequests(self::$contextId);
 
         // Check for roster
-        $hasRoster = \Tsugi\Core\LTIX::populateRoster(false);
-        if ($hasRoster) {
+        if (CommonService::$hasRoster) {
             // If there is a roster, learner list will be populated from it (such as when launched from LMS)
-            $rosterLearners = $GLOBALS['ROSTER']->data;
-            foreach ($rosterLearners as $learner) {
+            foreach (CommonService::$rosterData as $learner) {
                 foreach ($requests as $key => $request) {
                     if ($learner["role"] == 'Learner' && $learner['user_id'] == $request['user_id']) {
                         $requests[$key]['learner_name'] = $learner["person_name_family"] . ', ' . $learner["person_name_given"];
@@ -131,11 +129,9 @@ class InstructorCtr
         $calculatedUsage = self::$DAO->getKnownUsage(self::$contextId);
 
         // Check for roster
-        $hasRoster = \Tsugi\Core\LTIX::populateRoster(false);
-        if ($hasRoster) {
+        if (CommonService::$hasRoster) {
             // If there is a roster, learner list will be populated from it (such as when launched from LMS)
-            $rosterLearners = $GLOBALS['ROSTER']->data;
-            foreach ($rosterLearners as $learner) {
+            foreach (CommonService::$rosterData as $learner) {
                 foreach ($calculatedUsage as $key => $usage) {
                     if ($learner["role"] == 'Learner' && $learner['user_id'] == $usage['user_id']) {
                         $calculatedUsage[$key]['learner_name'] = $learner["person_name_family"] . ', ' . $learner["person_name_given"];
